d92305521663a8d0835f27a2d4ed55d680ccc774
YassinEldeeb <yassineldeeb94@gmail.com>
2022-03-29 00:41:52 +0200

    fix: disable window when launching

--- schedular/src/main.rs
+++ schedular/src/main.rs
@@ -1,4 +1,4 @@
-// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use std::{
     sync::{Arc, Mutex},
